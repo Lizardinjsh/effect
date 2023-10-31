55ed411976c4e73d50e2f1db478a1bd696b91277
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-10-31 09:52:37 +0200

    tes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
-import ToDo from "./ToDo.js"
+import { useEffect, useState } from "react";
+import ToDo from "./ToDo.js";
 
 function App() {
-  const toDo = {
-    userId: 1,
-    id: 1,
-    title: "delectus aut autem",
-    completed: false
+  const [toDo, setToDO] = useState({});
+  const [loading, setLoading];
+
+  useEffect(() => {  
+    async function getToDo() {
+      setLoading(true);
+    const response = await fetch("https://jsonplaceholder.typicode.com/todos/1"
+    );
+    const data = await response.json();
+    setToDo(data);
+    setLoading(false);
   };
+      getToDo();
+  }, []);
+
+ // const toDo = {
+  //   userId: 1,
+  //   id: 1,
+  //   title: "delectus aut autem",
+  //   completed: false
+
   return (
 <>
-  <ToDo {...toDo}/>
+  {loading}<ToDo {...toDo}/>
   </>
   );
 };
